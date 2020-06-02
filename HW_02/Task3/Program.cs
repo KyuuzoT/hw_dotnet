1b10bb58d582175bb841413f21d6287989a3f2e9
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-02 10:29:46 +0300

    Task3 - done

--- HW_02/Task3/Program.cs
+++ HW_02/Task3/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Task3
 {
@@ -10,6 +7,34 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+
+            /***
+             * Algorithm
+             * 1. Open text file as stream
+             * 2. Read all data from file to resulting string
+             * 3. Split resulting string using spaces as delimiters to get an 'array of substrings'
+             * 4. In cycle get each substring and convert it to byte
+             * 5. Make an array of such bytes
+             * 7. Write it to new image file.
+             * 
+             **/
+
+            byte binary;
+            using (StreamReader sr = new StreamReader(
+                @"D:\Development\Prog\HW_02\HW_02\Task3\Assets\image.txt", true))
+            {
+                string res = sr.ReadToEnd();
+                string[] arrRes = res.Split(' ');
+                byte[] imageBytes = new byte[arrRes.Length - 1];
+
+                for (int i = 0; i < arrRes.Length - 1; i++)
+                {
+                    binary = Convert.ToByte(arrRes[i], 2);
+                    imageBytes[i] = binary;
+                }
+                string sNameOfImage = "Screenshot.txt";
+                File.WriteAllBytes(@"D:\Development\Prog\HW_02\HW_02\Task3\Assets\" + sNameOfImage, imageBytes);
+            }
         }
     }
 }
