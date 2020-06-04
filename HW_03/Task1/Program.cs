5ac3c7018e887c69249ce013d46199e2b4bbdd01
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-04 23:51:05 +0300

    Menu added

--- HW_03/Task1/Program.cs
+++ HW_03/Task1/Program.cs
@@ -11,6 +11,29 @@ namespace Task1
             double secondValue;
             while (true)
             {
+                Console.WriteLine("This program could perform several actions:");
+                Console.WriteLine("\t1. Calculate sum of two numbers;");
+                Console.WriteLine("\t2. Calculate difference of two numbers;");
+                Console.WriteLine("\t3. Calculate product of two numbers;");
+                Console.WriteLine("\t4. Calculate quotient of two numbers;");
+                Console.WriteLine("\t5. Calculate area of circle using radius;");
+                Console.WriteLine("Please input number represents menu item: ");
+                switch (Console.Read())
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    default:
+                        Console.WriteLine("Input unressolved. Please try again!");
+                        break;
+                }
                 Console.WriteLine("Input parameters for calculating:\n");
                 InputTwoParametrs(out firstValue, out secondValue);
                 if(isNumber)
