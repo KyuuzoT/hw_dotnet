e33c924c2fe5d318ded541b8349ba067a716a995
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-05 10:52:11 +0300

    Added calculation of full age

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -10,15 +10,30 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+            int age;
+            DateTime currentDT = DateTime.Now;
             Console.WriteLine("Please input your name:");
-            string sName = Console.ReadLine();
-            DateTime birthday;
+            string sName = Console.ReadLine();            
+            DateTime birthdayDT;
             do
             {
                 Console.WriteLine("Please input your date of birth in format: {0:d}", new DateTime(1970,1,1));
-            } while (!DateTime.TryParse(Console.ReadLine(), out birthday));
-            
-            
+            } while (!DateTime.TryParse(Console.ReadLine(), out birthdayDT));
+
+            Console.WriteLine(currentDT);
+            age = GetAge(birthdayDT, currentDT);
+            //Console.WriteLine($"Hello, {sName}! Your age is {age}!");
+            Console.Read();
+        }
+
+        private static int GetAge(DateTime birthdayDT, DateTime currentDT)
+        {
+            int years = currentDT.Year - birthdayDT.Year;
+            if(currentDT.DayOfYear < birthdayDT.DayOfYear)
+            {
+                years--;
+            }
+            return years;
         }
     }
 }
