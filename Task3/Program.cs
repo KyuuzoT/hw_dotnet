09333a95ecae871fea32010947406efe2144a324
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-05 10:59:09 +0300

    Added happy birthday!

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -8,6 +8,7 @@ namespace Task3
 {
     class Program
     {
+        static bool bHappyBirthday;
         static void Main(string[] args)
         {
             int age;
@@ -22,17 +23,28 @@ namespace Task3
 
             Console.WriteLine(currentDT);
             age = GetAge(birthdayDT, currentDT);
-            //Console.WriteLine($"Hello, {sName}! Your age is {age}!");
+            Console.WriteLine($"Hello, {sName}! Your age is {age}!");
+            if(bHappyBirthday)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"So, {sName}, today is your birthday! We wish you best luck!");
+                Console.ResetColor();
+            }
             Console.Read();
         }
 
         private static int GetAge(DateTime birthdayDT, DateTime currentDT)
         {
+            bHappyBirthday = false;
             int years = currentDT.Year - birthdayDT.Year;
             if(currentDT.DayOfYear < birthdayDT.DayOfYear)
             {
-                years--;
+                years--;                
             }
+            else if(currentDT.DayOfYear == birthdayDT.DayOfYear)
+            {
+                bHappyBirthday = true;
+            }            
             return years;
         }
     }
