3b5f46146032ee0f15c27c4c276593b20c41c368
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-08 11:33:01 +0300

    HW_04 Task6 - added player reaction

--- HW_04/Task6/Program.cs
+++ HW_04/Task6/Program.cs
@@ -10,7 +10,32 @@ namespace Task6
     {
         static void Main(string[] args)
         {
-            
+            while (true)
+            {
+                ConsoleKeyInfo input = Console.ReadKey();
+                Console.WriteLine();
+                switch (input.Key)
+                {
+                    case ConsoleKey.W:
+                        Console.WriteLine("Player moves upwards");
+                        break;
+                    case ConsoleKey.S:
+                        Console.WriteLine("Player moves downwards");
+                        break;
+                    case ConsoleKey.A:
+                        Console.WriteLine("Player moves leftwards");
+                        break;
+                    case ConsoleKey.D:
+                        Console.WriteLine("Player moves rightwards");
+                        break;
+                    case ConsoleKey.Escape:
+                        return;
+                    default:
+                        Console.WriteLine("Player stays where he is.");
+                        break;
+                }
+            }
+
         }
     }
 }
