fb7c6f42a35e8a7cc44b9f01f85e605ec803e3c8
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-08 11:25:25 +0300

    HW_04 Task5 - done!

--- HW_04/Task5/Program.cs
+++ HW_04/Task5/Program.cs
@@ -10,6 +10,12 @@ namespace Task5
     {
         static void Main(string[] args)
         {
+            for (int i = 0x5A, j = 0x7A; i >= 0x41 && j >= 0x61; i--, j--)
+            {
+                Console.Write(Char.ConvertFromUtf32(i) + Char.ConvertFromUtf32(j) + " ");
+            }
+
+            Console.ReadKey();
         }
     }
 }
