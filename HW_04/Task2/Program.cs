660c12bff1d42366dd75aa813c1f291258e08ac6
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-08 10:04:10 +0300

    HW_04 Task2 - added methods from Task1

--- HW_04/Task2/Program.cs
+++ HW_04/Task2/Program.cs
@@ -10,6 +10,38 @@ namespace Task2
     {
         static void Main(string[] args)
         {
+            SumOfNumbers();
+            Console.ReadKey();
         }
+
+        private static void SumOfNumbers()
+        {
+            double firstVal;
+            double secondVal;
+            firstVal = InputValue("first");
+            secondVal = InputValue("second");
+
+            Console.WriteLine($"Sum of {firstVal} and {secondVal} equals to {firstVal + secondVal}");
+        }
+
+        private static double InputValue(string strVar)
+        {
+            double value = 0;
+            string input;
+            while (true)
+            {
+                Console.WriteLine($"Please, input {strVar} variable:");
+                input = Console.ReadLine();
+                if (Double.TryParse(input, out value))
+                {
+                    return value;
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is not a valid number!");
+                }
+            }
+        }
+
     }
 }
