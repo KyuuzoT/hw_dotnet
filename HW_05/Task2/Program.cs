997b228a3844bb1a9b7fb92dad34d67b3a8188e1
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-10 11:02:59 +0300

    HW_05 Task2 - added method for excluding oune of indexes

--- HW_05/Task2/Program.cs
+++ HW_05/Task2/Program.cs
@@ -4,9 +4,19 @@ namespace Task2
 {
     class Program
     {
+        static int ArraySize = 10;
+        static int ExcludedIndex = 0;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double[] inputArray = new double[ArraySize];
+            Console.ReadKey();
+        }
+
+
+        private static void GetExcludedIndex(int size, ref int excludedIndex)
+        {
+            Random rnd = new Random();
+            excludedIndex = rnd.Next(0, size);
         }
     }
 }
