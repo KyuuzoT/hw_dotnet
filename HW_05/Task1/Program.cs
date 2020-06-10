bc921b5b63f260e99bc0bee5bab87a4df4a5a7cc
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-10 10:31:27 +0300

    HW_05 Task1 - added method for filling array with randoms

--- HW_05/Task1/Program.cs
+++ HW_05/Task1/Program.cs
@@ -11,10 +11,21 @@ namespace Task1
             double[] inputArray = new double[ArraySize];
             double[] sumArray = new double[ArraySize];
 
-            
+            ArrayFillWithRandoms(out randomArray);
+
             PrintArray(randomArray);
             Console.ReadKey();
-        }        
+        }
+
+        private static void ArrayFillWithRandoms(out double[] array)
+        {
+            array = new double[ArraySize];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(1, 999);
+            }
+        }
 
         private static void PrintArray(double[] array)
         {
