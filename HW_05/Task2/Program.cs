11717a9d0e570c3f3e6a02c79556c11a7aa95a77
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-10 11:10:30 +0300

    HW_05 Task2 - added method for filling an array

--- HW_05/Task2/Program.cs
+++ HW_05/Task2/Program.cs
@@ -9,9 +9,33 @@ namespace Task2
         static void Main(string[] args)
         {
             double[] inputArray = new double[ArraySize];
+            FillArrayWithInput(out inputArray, ArraySize);
+
             Console.ReadKey();
         }
 
+        private static void FillArrayWithInput(out double[] array, int size)
+        {
+            array = new double[size];
+            string tempStr;
+            GetExcludedIndex(size, ref ExcludedIndex);
+            Console.WriteLine($"Please, input {size - 1} numbers to fill array:");
+            for (int i = 0; i < size; i++)
+            {
+                if(i==ExcludedIndex)
+                {
+                    Console.WriteLine($"{ExcludedIndex} was excluded!");
+                    continue;
+                }
+                Console.Write($"{i} number: ");
+                tempStr = Console.ReadLine();
+                if(!Double.TryParse(tempStr, out array[i]))
+                {
+                    Console.WriteLine("Wrong input! Try again!");
+                    i--;
+                }
+            }
+        }
 
         private static void GetExcludedIndex(int size, ref int excludedIndex)
         {
