54cccbe3f844a5df32500ea576ba76665c5d893e
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-10 10:07:01 +0300

    Task1 HW_05 - project renamed

---
+++ HW_05/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        private static int ArraySize = 10;
+        static void Main(string[] args)
+        {
+            double[] randomArray = new double[ArraySize];
+            double[] inputArray = new double[ArraySize];
+            double[] sumArray = new double[ArraySize];
+
+            ArrayFillWithRandoms(out randomArray);
+
+            Console.ReadKey();
+        }
+
+        private static void ArrayFillWithRandoms(out double[] array)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
