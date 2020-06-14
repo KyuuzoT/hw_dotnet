59576a5c8ef8d7a28659aa3f4ce4bda35b45b442
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-14 16:14:04 +0300

    HW_05 Additional Task - added method for calculating mean value

--- Task4Additional/Program.cs
+++ Task4Additional/Program.cs
@@ -9,28 +9,56 @@ namespace Task4Additional
 {
     class Program
     {
-        const long ArraySize = 100_000_000;
+        const long ArraySize = 1_000_000;
+        const int triesArraySize = 10;
         static void Main(string[] args)
         {
             var timer = new Stopwatch();
             double[] initArray;
-            timer.Start();
-            FillArray(out initArray);
-            timer.Stop();
-            Console.WriteLine($"Filling an array time: {timer.ElapsedMilliseconds}");
+            double[] revArray;
+            double[] sysArray;
+            long[] elapsedFilling = new long[triesArraySize];
+            long[] elapsedMyRev = new long[triesArraySize]; ;
+            long[] elapsedSysRev = new long[triesArraySize]; ;
+            for (int i = 0; i < triesArraySize; i++)
+            {
+                timer.Start();
+                FillArray(out initArray);
+                timer.Stop();
+                sysArray = initArray;
+                Console.WriteLine($"Filling an array time: {timer.ElapsedMilliseconds}");
+                elapsedFilling[i] = timer.ElapsedMilliseconds;
 
-            timer.Start();
-            double[] revArray = ReverseArray(initArray);
-            timer.Stop();
-            Console.WriteLine($"Time of reversing an array with my method equals to {timer.ElapsedMilliseconds}");
+                timer.Start();
+                revArray = ReverseArray(initArray);
+                timer.Stop();
+                Console.WriteLine($"Time of reversing an array with my method equals to {timer.ElapsedMilliseconds}");
+                elapsedMyRev[i] = timer.ElapsedMilliseconds;
 
-            double[] sysArray = initArray;
-            timer.Start();
-            Array.Reverse(sysArray);
-            timer.Stop();
-            Console.WriteLine($"Time of reversing an array with system method equals to {timer.ElapsedMilliseconds}");
+                
+                timer.Start();
+                Array.Reverse(sysArray);
+                timer.Stop();
+                Console.WriteLine($"Time of reversing an array with system method equals to {timer.ElapsedMilliseconds}");
+                elapsedSysRev[i] = timer.ElapsedMilliseconds;
+                timer = new Stopwatch();
+            }
 
+            Console.WriteLine($"Mean time: \n Filling = {MeanValue(elapsedFilling)}" +
+                $"\n My reverse = {MeanValue(elapsedMyRev)}" +
+                $"\n System reverse = {MeanValue(elapsedSysRev)}");
             Console.ReadKey();
+        }
+
+        private static long MeanValue(long[] array)
+        {
+            long sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return (sum / array.Length);
         }
 
         private static void FillArray(out double[] initArray)
