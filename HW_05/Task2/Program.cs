3a3859177130e8fb5e07d0eeb604fd28b5eb4708
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-14 15:21:40 +0300

    Task2 - done

--- HW_05/Task2/Program.cs
+++ HW_05/Task2/Program.cs
@@ -9,42 +9,75 @@ namespace Task2
         static bool AlreadyExcluded = false;
         static void Main(string[] args)
         {
+            double excludedNumber;
             double[] inputArray = new double[ArraySize];
-            FillArrayWithInput(out inputArray, ArraySize);
+            GetExcludedIndex(ref ExcludedIndex);
+            GetNumberOnExcludedIndex(out excludedNumber);
 
+            FillArrayWithInput(out inputArray, ArraySize);
             PrintArray(inputArray);
+
+
+
+            PrintArray(ShiftArray(inputArray, excludedNumber), "Shifted array");
             Console.ReadKey();
+        }
+
+        private static double[] ShiftArray(double[] array, double number)
+        {
+            double[] outputArray = new double[array.Length];
+            for (int i = 0, j = 0; i < outputArray.Length; i++)
+            {
+                if(i == ExcludedIndex)
+                {
+                    outputArray[i] = number;
+                    continue;
+                }
+
+                outputArray[i] = array[i];
+            }
+
+            return outputArray;
         }
 
         private static void FillArrayWithInput(out double[] array, int size)
         {
             array = new double[size];
-
-            GetExcludedIndex(size, ref ExcludedIndex);
             Console.WriteLine($"Please, input {size - 1} numbers to fill array:");
-            for (int i = 0; i < size - 1; i++)
+            for (int i = 0; i < size; i++)
             {
                 if (!AlreadyExcluded && i == ExcludedIndex)
                 {
                     Console.WriteLine($"{ExcludedIndex} was excluded!");
                     AlreadyExcluded = true;
+                    array[i] = double.NaN;
+                    continue;
                 }
-
                 GetInputNumber(out array[i], ref i);
             }
+            Console.WriteLine();
+        }
+
+        private static void GetNumberOnExcludedIndex(out double number)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please, input the number you want to insert in array.");
+                if (!Double.TryParse(Console.ReadLine(), out number))
+                {
+                    Console.WriteLine("Wrong input! Try again, please!");
+                }
+                else
+                {
+                    break;
+                }
+            }           
         }
 
         private static void GetInputNumber(out double number, ref int position)
         {
             string tempStr;
-            if (AlreadyExcluded)
-            {
-                Console.Write($"{position + 1} number: ");
-            }
-            else
-            {
-                Console.Write($"{position} number: ");
-            }
+            Console.Write($"{position} number: ");
 
             tempStr = Console.ReadLine();
             if (!Double.TryParse(tempStr, out number))
