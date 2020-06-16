e4ec54ba94c22e7fe090494aebd43e0e35904c19
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-16 20:35:27 +0300

    HW_06 Task2 - done!

--- HW_06/Task2/Program.cs
+++ HW_06/Task2/Program.cs
@@ -16,9 +16,8 @@ namespace Task2
             string[] textArray;
             textArray = StringToArray(text);
             Utils.PrintArray(textArray, true);
-
-            string sorted = Utils.BuildString(textArray);
-            RemoveLongestWord(sorted);
+            
+            RemoveLongestWord(text);
 
             SwapLongestShortest(text);
             CountSymbols(text);            
@@ -29,6 +28,7 @@ namespace Task2
         private static void RemoveLongestWord(string text)
         {
             int spaceIndex;
+            string sorted = Utils.BuildString(StringToArray(text));
             for (spaceIndex = 0; spaceIndex < text.Length; spaceIndex++)
             {
                 if(text[spaceIndex] == ' ')
@@ -42,10 +42,32 @@ namespace Task2
 
         private static void SwapLongestShortest(string text)
         {
+            string[] textArray = StringToArray(text);
+            Utils.PrintArray(textArray, "\nBefore swap", false);
+            string temp;
+            temp = textArray[0];
+            textArray[0] = textArray[textArray.Length - 1];
+            textArray[textArray.Length - 1] = temp;
+            Console.WriteLine($"\n\nSwap Longest word with shortest word:\n {Utils.BuildString(textArray)}");
         }
 
         private static void CountSymbols(string text)
         {
+            int lettersCounter = 0;
+            int marksCounter = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Char.IsLetter(text[i]))
+                {
+                    lettersCounter++;
+                }
+                else if (Char.IsPunctuation(text[i]))
+                {
+                    marksCounter++;
+                }
+            }
+
+            Console.WriteLine($"\nLetters quantity:{lettersCounter}\nPunctuation marks quantity:{marksCounter}");
         }
 
         private static string[] StringToArray(string text)
