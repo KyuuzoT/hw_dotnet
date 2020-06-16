af5b0ae8af86c8435ee1322d832337fbd6ff9fb9
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-16 21:16:41 +0300

    HW_06 Additional task started!

---
+++ HW_06/Task4Additional/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4Additional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object objInt = 132;
+            object objString = "smth";
+            Console.WriteLine($"Test int-box: {objInt} string-box: {objString}  {18}");
+            Console.WriteLine("Test int-box: " + objInt + " string-box: " + objString + "  " + 18);
+
+            Console.ReadKey();
+        }
+    }
+}
