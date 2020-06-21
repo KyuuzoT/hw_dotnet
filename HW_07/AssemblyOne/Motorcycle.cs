14937b45c586e76e9c276bfc6dea7e2a2f235e46
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-21 19:39:48 +0300

    HW_07 Task1 - Motorcycle class done!

--- HW_07/AssemblyOne/Motorcycle.cs
+++ HW_07/AssemblyOne/Motorcycle.cs
@@ -8,9 +8,59 @@ namespace AssemblyOne
 {
     class Motorcycle
     {
-        private const UInt16 _maxSpeedPrivate = 180;
+        private const UInt16 _maxSpeed = 180;
         private protected string _vinNumber;
-        internal int _odometer
+        internal int _odometer;
         protected internal int _engineVolume;
+        public string CountryOfOrigin;
+        protected bool isRedisigned;
+
+        public ushort MaxSpeed
+        {
+            get
+            {
+                return _maxSpeed;
+            }
+        }
+
+        public string VinNumber
+        {
+            get
+            {
+                if(String.IsNullOrEmpty(_vinNumber))
+                {
+                    _vinNumber = Guid.NewGuid().ToString();
+                }
+                return _vinNumber;
+            }
+            set
+            {
+                _vinNumber = value;
+            }
+        }
+
+        public Motorcycle()
+        {
+            _vinNumber = "00000000-0000-0000-0000-000000000000";
+            _odometer = 5_000;
+            _engineVolume = 50;
+            CountryOfOrigin = "China";
+            isRedisigned = false;
+        }
+
+        protected void StartEngine()
+        {
+            Console.WriteLine("Engine goes \"Wroom-Wroom\"!");
+        }
+
+        internal void StopEngine()
+        {
+            Console.WriteLine("Engine goes \"Tphruuuuum\"! And... silence!");
+        }
+
+        protected internal void TurnOnCoolMusic()
+        {
+            Console.WriteLine("♫♪♫Back in black\nI hit the sack\nI’ve been too long I’m glad to be back!♫♪♫");
+        }
     }
 }
