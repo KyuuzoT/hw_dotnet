5ae1a1b514f082342c7a637d70b197b6f3bca127
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-21 19:18:19 +0300

    HW_07 Task1 - started

---
+++ HW_07/AssemblyOne/Motorcycle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyOne
+{
+    class Motorcycle
+    {
+        private const UInt16 _maxSpeedPrivate = 180;
+        private protected string _vinNumber;
+        internal int _odometer
+        protected internal int _engineVolume;
+    }
+}
