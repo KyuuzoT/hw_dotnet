7f98300c4c461d10e3bbbce1dd238d54480de455
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-21 21:27:26 +0300

    HW_07 Task2 - done!

--- HW_07/StringLettersCounting/Program.cs
+++ HW_07/StringLettersCounting/Program.cs
@@ -8,8 +8,53 @@ namespace StringLettersCounting
 {
     class Program
     {
+        static SortedDictionary<char, int> Letters = new SortedDictionary<char, int>();
+
         static void Main(string[] args)
         {
+            string input;
+            StringBuilder builder = new StringBuilder();
+            Console.WriteLine("Input string for letters counting:");
+            input = Console.ReadLine();
+            if(string.IsNullOrEmpty(input))
+            {
+                input = "This is some sort of test string, bouya";
+            }
+
+            ParseInputString(input);
+            foreach (var pair in Letters)
+            {
+                builder.Append(pair.Value).Append(pair.Key);
+            }
+
+            Console.WriteLine($"Result of counting: \"{builder.ToString()}\"");
+            Console.ReadKey();
+        }
+
+        private static void ParseInputString(string text)
+        {
+            text = text.ToLower();
+            foreach (char ch in text)
+            {
+                if(char.IsLetter(ch))
+                {
+                    CountLettersInText(ch);
+                }
+            }
+        }
+
+        private static void CountLettersInText(char ch)
+        {
+            int value;
+            if(Letters.TryGetValue(ch,out value))
+            {
+                Letters[ch]++;
+            }
+            else
+            {
+                value = 1;
+                Letters.Add(ch, value);
+            }
         }
     }
 }
