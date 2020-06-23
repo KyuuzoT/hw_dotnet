6cf687dff5600f61f76bf7d5068869430dbcf8a9
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-23 19:36:28 +0300

    HW_07 Task1 - done!

--- HW_07/AssemblyOne/Motorcycle.cs
+++ HW_07/AssemblyOne/Motorcycle.cs
@@ -2,14 +2,14 @@
 
 namespace AssemblyOne
 {
-    class Motorcycle
+    public class Motorcycle
     {
         private const UInt16 _maxSpeed = 180;
         private protected string _vinNumber;
         internal int _odometer;
-        protected internal int _engineVolume;
-        public string CountryOfOrigin;
-        protected bool isRedisigned;
+        protected internal int _engineVolume = 1;
+        public string CountryOfOrigin = "Serinistan";
+        protected bool isRedisigned = false;
 
         public ushort MaxSpeed
         {
@@ -31,7 +31,14 @@ namespace AssemblyOne
             }
             set
             {
-                _vinNumber = value;
+                if (!String.IsNullOrEmpty(value))
+                {
+                    _vinNumber = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid vin-number!");
+                }
             }
         }
 
@@ -59,7 +66,7 @@ namespace AssemblyOne
         protected internal void TurnOnCoolMusic()
         {
             Console.WriteLine("Cool Music:");
-            Console.WriteLine("\t♫♪♫Back in black\t\nI hit the sack\t\nI’ve been too long I’m glad to be back!♫♪♫");
+            Console.WriteLine("\t♫♪♫Back in black\t\nI hit the sack\t\n I’ve been too long I’m glad to be back!♫♪♫");
         }
     }
 }
