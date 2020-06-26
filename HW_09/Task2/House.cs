f46c512ae483700df832d19c8b8783fdba2bc365
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-26 11:19:14 +0300

    HW_09 Task2 - started!

---
+++ HW_09/Task2/House.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class House
+    {
+        private float _area;
+
+        public House()
+        {
+            _area = 200.0f;
+        }
+
+        public House(int area)
+        {
+            _area = area;
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine($"I'm a {this.GetType()}, my area equals to {_area} m²");
+        }
+    }
+}
