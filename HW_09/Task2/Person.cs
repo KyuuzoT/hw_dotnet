614352bc4604cd424c2697923cbe3e19de9e4dac
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-26 12:16:07 +0300

    HW_09 Task2 - Person class finished

--- HW_09/Task2/Person.cs
+++ HW_09/Task2/Person.cs
@@ -8,6 +8,43 @@ namespace Task2
 {
     class Person
     {
+        private House _personalHouse;
+        public string Name
+        {
+            get; set;
+        }
 
+        public House HouseOfPerson
+        {
+            get { return _personalHouse; }
+            set
+            {
+                if(value != null)
+                {
+                    _personalHouse = value;
+                }
+                else
+                {
+                    Console.WriteLine("Every person should have a house!");
+                }
+            }
+        }
+
+        public Person()
+        {
+            Name = "Gzegosh Breshtashikevich";
+        }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine($"Hi, I'm {this.GetType()}, my name is {this.Name}, I live in {_personalHouse.GetType()} with this parameters: ");
+            _personalHouse.ShowData();
+            _personalHouse.GetDoor().ShowData();
+        }
     }
 }
