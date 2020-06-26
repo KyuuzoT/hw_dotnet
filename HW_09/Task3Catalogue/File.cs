5bb3da84bb4b75f64f076bbd59519c9144bc9c1e
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-26 13:03:01 +0300

    HW_09 Task3 - added basic file properties

--- HW_09/Task3Catalogue/File.cs
+++ HW_09/Task3Catalogue/File.cs
@@ -8,5 +8,9 @@ namespace Task3Catalogue
 {
     class File
     {
+        private string _name;
+        private Guid _idCode;
+        private string _category;
+        private long _size;
     }
 }
