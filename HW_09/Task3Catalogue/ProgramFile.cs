1e69ef94dd95814636ebd84c779fb2529080e2a7
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-28 10:26:14 +0300

    HW_09 Task3 - ProgramFile class finished.

--- HW_09/Task3Catalogue/ProgramFile.cs
+++ HW_09/Task3Catalogue/ProgramFile.cs
@@ -8,5 +8,34 @@ namespace Task3Catalogue
 {
     class ProgramFile
     {
+        protected long _size;
+
+        public long Size
+        {
+            get
+            {
+                return _size;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _size = value;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Size of file can not be negative and must be greater than 0!");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        public void Execute()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Not implemented yet.");
+            Console.ResetColor();
+        }
     }
 }
