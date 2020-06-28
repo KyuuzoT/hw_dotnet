9a067040e6fbe4c543e09db31f00cd29be6fe2d1
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-28 10:20:53 +0300

    HW_09 Task3 - MusicFile class finished!

--- HW_09/Task3Catalogue/MusicFile.cs
+++ HW_09/Task3Catalogue/MusicFile.cs
@@ -6,7 +6,69 @@ using System.Threading.Tasks;
 
 namespace Task3Catalogue
 {
-    class MusicFile
+    class MusicFile : File
     {
+        protected string _singer;
+        protected int _duration;
+        
+        public string Singer
+        {
+            get
+            {
+                return _singer;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _singer = value;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Singers usually have names!");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        public int Duration
+        {
+            get
+            {
+                return _duration;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _duration = value;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Duration can not be negative and must be greater than 0!");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        public void Play()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Not implemented yet.");
+            Console.ResetColor();
+        }
+
+        public void RetrieveInformation()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Establishing Internet connection. Stand by.");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Not implemented yet.");
+            Console.ResetColor();
+        }
     }
 }
