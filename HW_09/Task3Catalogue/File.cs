57f9604aa77c3684f9cc8ef130626b09d8dca70b
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-28 10:03:37 +0300

    HW_09 Task3 - File class finished

--- HW_09/Task3Catalogue/File.cs
+++ HW_09/Task3Catalogue/File.cs
@@ -8,9 +8,89 @@ namespace Task3Catalogue
 {
     class File
     {
-        private string _name;
-        private Guid _idCode;
-        private string _category;
-        private long _size;
+        protected string _name;
+        protected Guid _idCode;
+        protected string _category;
+        protected long _size;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if(!string.IsNullOrEmpty(value))
+                {
+                    _name = value;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Name could not be empty!");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        public Guid ID
+        {
+            get
+            {
+                if (_idCode.Equals(Guid.Empty) || string.IsNullOrEmpty(_idCode.ToString()))
+                {
+                    return Guid.NewGuid();
+                }
+
+                return _idCode;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value.ToString()))
+                {
+                    _idCode = value;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You must set up a valid ID!");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        public string Category
+        {
+            get
+            {
+                return _category;
+            }
+            set
+            {
+                _category = value;
+            }
+        }
+
+        public long Size
+        {
+            get
+            {
+                return _size;
+            }
+            set
+            {
+                if(value > 0)
+                {
+                    _size = value;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Size can not be negative!");
+                    Console.ResetColor();
+                }
+            }
+        }
     }
 }
