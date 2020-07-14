5e6f45378e7c4cea3cf271c501a1f2ba00284283
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-07-14 18:55:26 +0300

    Task 3 - Created class repository

---
+++ HW_12/Task 12-3/MotorcycleRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_12_3
+{
+    class MotorcycleRepository : IRepository<Motorcycle>
+    {
+
+        public void CreateMotorcycle(Motorcycle item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteMotorcycle(Guid ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteMotorcycle(Motorcycle item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Motorcycle GetMotorcycleById(Guid ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Motorcycle> GetMotorcycles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateMotorcycle(Motorcycle item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
