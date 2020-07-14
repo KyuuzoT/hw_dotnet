377422d0e53d999b3e79005df74ecacf1c016737
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-07-14 18:53:44 +0300

    Task3 added repository pattern interface

---
+++ HW_12/Task 12-3/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_12_3
+{
+    interface IRepository <T> where T:class
+    {
+        IEnumerable<T> GetMotorcycles();
+        T GetMotorcycleById(Guid ID);
+        void CreateMotorcycle(T item);
+        void UpdateMotorcycle(T item);
+        void DeleteMotorcycle(Guid ID);
+        void DeleteMotorcycle(T item);
+    }
+}
---
+++ HW_12/Task 12-3/Motorcycle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_12_3
+{
+    class Motorcycle
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public short Year { get; set; }
+        public int OdometrValue { get; set; }
+    }
+}
