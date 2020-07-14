3d56b8cb3665e8d63de1b48c135c6e1c5ca0747e
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-07-14 18:20:02 +0300

    Task2 - added enum of genres

---
+++ HW_12/Task 12-2/Genres.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_12_2
+{
+    enum Genres
+    {
+        Rock,
+        PunkRock,
+        HardRock,
+        ClassicRock,
+        Metal,
+        SymphonicMetal,
+        DeathMetal,
+        FolkMetal
+    }
+}
