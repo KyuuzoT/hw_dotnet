91bfb1aacb65ec4af5a19885cfdfa8b593a29e04
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-07-14 18:04:44 +0300

    Task2 - started

---
+++ HW_12/Task 12-2/Song.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_12_2
+{
+    class Song
+    {
+        private string _name;
+        private int _duration;
+        private string _author;
+        private DateTime _releaseDate;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                CheckString(value, out _name);
+            }
+        }
+
+        public string Author
+        {
+            get => _author;
+            set
+            {
+                CheckString(value, out _author);
+            }
+        }
+
+        public DateTime ReleaseDate
+        {
+            get => _releaseDate;
+            set
+            {
+                if(value == default)
+                {
+                    Console.WriteLine("What's this? Ancient release? Suggest you to change it.");
+                }
+                else
+                {
+                    _releaseDate = value;
+                }
+            }
+        }
+
+        private static void CheckString(string value, out string destination)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                Console.WriteLine("This field could not be empty!");
+            }
+            destination = value;
+        }
+    }
+}
